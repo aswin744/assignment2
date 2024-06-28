6f5a5f48e6989f6837ff58c8adcac0eb83395639
aswin744 <71869283+aswin744@users.noreply.github.com>
2024-06-28 21:19:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/users/create', 'users.create_user');
-Route::view('/users', 'users.display_users');
-Route::view('/users/{id}/edit', 'users.update_user')->name('edit_user');
+Route::view('/users/create', 'users.create');
+Route::view('/users', 'users.index');
+Route::view('/users/{id}/edit', 'users.edit')->name('edit_user');
