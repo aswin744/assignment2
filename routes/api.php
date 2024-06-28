45a7f28b63905defef55306030d87e9aee99c461
aswin744 <71869283+aswin744@users.noreply.github.com>
2024-06-28 21:21:11 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\API\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
