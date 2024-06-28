91cdb1b80988e8afa4eadeac6db184bee9b0d3db
aswin744 <71869283+aswin744@users.noreply.github.com>
2024-06-28 21:00:45 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('me', [AuthController::class, 'me']);
+Route::get('users', [UserController::class, 'index']);
 Route::post('/users','API\UserController@store');
 Route::get('/users','API\UserController@index');
 Route::put('/users/{id}','API\UserController@update');
